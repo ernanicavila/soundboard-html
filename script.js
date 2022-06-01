8ca83b0f70ce10d1e9e836dc3c4811a2ddc53557
Ernani Ávila <ernanicavilaneto@gmail.com>
2022-06-01 00:33:11 -0300

    added some features and css into project

---
+++ script.js
@@ -0,0 +1,31 @@
+const bender = new Audio();
+bender.src = "stuff/bender.mp3";
+
+const probleminha = new Audio();
+probleminha.src = "stuff/probleminha.mp3";
+
+const bixao = new Audio()
+bixao.src='stuff/bixao.mpeg'
+
+const loco = new Audio()
+loco.src='stuff/loco.mpeg'
+
+const en = document.querySelector(".bender");
+en.addEventListener("click", () => {
+  bender.play();
+});
+
+const problema = document.querySelector(".probleminha");
+problema.addEventListener("click", () => {
+  probleminha.play();
+});
+
+const bixo = document.querySelector('.bixao')
+bixo.addEventListener('click', ()=>{
+  bixao.play();
+})
+
+const looko = document.querySelector('.loko')
+looko.addEventListener('click', ()=>{
+  loco.play()
+})
